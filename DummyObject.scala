e91b783ebb56c6237d9637641fd1ca0586805dd4
Sankar Biswas <sankarb475@gmail.com>
2018-12-31 15:30:09 +0530

    Update DummyObject.scala

--- DummyObject.scala
+++ DummyObject.scala
@@ -3,7 +3,7 @@ package com.netflix.movies.utility
 object DummyObject extends java.io.Serializable {
   
   def getOutput(inputParam1 : String, inputParam2 : String, inputParam3 : String) : Option[String] = {
-    Option(inputParam1+ " " + inputParam2 + " " + " " + inputParam3)
+    Option(inputParam1+ " " + inputParam2 + " " + inputParam3)
   }
 
 }
